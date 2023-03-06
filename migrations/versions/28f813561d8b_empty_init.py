247e66c7488e1cad4c87008668ca31c0d653da19
Robert Gorz <rjgorz@gmail.com>
2023-03-06 15:50:08 -0600

    Completed

---
+++ migrations/versions/28f813561d8b_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 28f813561d8b
+Revises: 
+Create Date: 2023-03-06 15:36:19.504626
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '28f813561d8b'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import create_engine, Column, DateTime, Integer, String, desc
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
+
+engine = create_engine('sqlite:///migrations_test.db')
+
+Base = declarative_base()
+
+class Student(Base):
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(), index=True)
+    email = Column(String(55))
+    grade = Column(Integer())
+    birthday = Column(DateTime())
+    enrolled_date = Column(DateTime(), default=datetime.now())
+
+    def __repr__(self):
+        return f"Student {self.id}: " \
+            + f"{self.name}" \
+            + f"{self.grade}"
